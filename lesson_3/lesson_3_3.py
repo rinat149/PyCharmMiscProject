b7bdc747795374c97775f1a4bc83fe03e646df23
Rinat Vagapov <rvagapov550@gmail.com>
2025-12-21 16:27:28 +0700

    lesson 3,4

---
+++ lesson_3/lesson_3_3.py
@@ -0,0 +1,14 @@
+# s = "Hello, rinat"
+#
+# """"
+# 0  1  2  3  4  5  6  7  8  9  10
+# H  e  l  l  o  ,  r  i  n  a  t
+# """
+#
+# # print(s[-1])
+#
+# # print("string"[3])
+#
+# # print(s[7:])
+# # print(s[:5])
+# print(s[:])
---
+++ lesson_4/lesson_4_1.py
@@ -0,0 +1,36 @@
+from shlex import split
+from turtledemo.penrose import start
+
+# s = "Rinat Vagapov"
+# # s1 = s.upper()
+# # s2 = s.lower()
+# # # print(s.lower())
+# # # print(s.upper())
+# #
+# # print(s.count( x: "i",_start))
+# # print(s.count("i", 0, 6))
+# print(s.isspace())
+# a = "22"
+# b = "223535"
+# c = "2223424"
+# print(a.rjust(6))
+# print(b.rjust(6))
+# print(c.rjust(6))
+
+# s = "Николаев-Даниил-Александрович"
+# name, surname, second_name = s.split("-")
+# print(name)
+# print(surname)
+# print(second_name)
+
+# nums = "1, 2,43,   45, 444,   34"
+# print(nums.replace(" ", "").split(','))
+
+# words = ['str', 'float', 'bool']
+# print(', '.join(words))
+
+# a = "  aaaa  aaaa   "
+# print(a.strip())
+# print(a.lstrip())
+# print(a.rstrip())
+

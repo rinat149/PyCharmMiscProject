2238a756fbfc967474540bb3ee3a33785b1229a7
Rinat Vagapov <rvagapov550@gmail.com>
2025-12-25 21:10:17 +0700

    lesson 9

---
+++ lesson_9/lesson_9_1.py
@@ -0,0 +1,45 @@
+# dict_ex = ["Rinat", "Vagapov", "35"]
+dict_ex = {"name": "Rinat", "lastname": "Vagapov", "age": 35 }
+# #
+# print(dict_ex["age "])
+
+# dict_ex = dict(name = "Rinat", last_name = "Vagapov")
+#
+# print(dict_ex)
+
+# dict_ex =[["name", "Rinat"], ["last_name", "Vagapov"]]
+# print(dict(dict_ex))
+#
+# """
+# Ключами могут быть:
+# str
+# int
+# bool
+# tuple
+#
+# """
+
+# dict_ex = dict.fromkeys(["Rinat", "Vagapov", "35"])
+# print(dict_ex)
+#
+
+# name = dict_ex.get("name")
+# print(name)
+
+# dict_ex.setdefault('name', 'Rinat')
+# print(dict_ex)
+
+
+# str_1 = dict_ex.pop("name")
+# print(dict_ex)
+
+# print(dict_ex)
+# item = dict_ex.popitem()
+# print(item)
+#
+# print(dict_ex)
+
+# print(list(dict_ex.values()))
+# for k, v in dict_ex.items():
+#     print(k, v)
+
---
+++ lesson_9/lesson_9_2.py
@@ -0,0 +1,35 @@
+# t = (1,2)
+# print(type(t))
+
+
+t = (1,53535, 53535, 6464, 'rw', [44, 54, 455])
+# print(t)
+#
+# print(t[3])
+
+l = (1,53535, 53535, 6464, 'rw', [44, 54, 455])
+
+# print(t.__sizeof__())
+# print(l.__sizeof__())
+
+# t[5][0] = 7
+# print(t)
+#
+# t2 = (3434, 53535)
+# t3 = t + t2
+# print(t3)
+
+# t4 = (0,) * 4
+# print(t4)
+
+# l = list(t)
+# print(l)
+# t2 = tuple(l)
+# print(t2)
+
+# t6 = tuple("hello")
+# print(t6)
+
+# print(t[5].append(t[5]))
+# print(t[5].append(t[5]))
+# print(t[5].append(t[5]))

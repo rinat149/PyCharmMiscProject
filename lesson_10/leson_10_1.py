d855e51937a27a6260940306968aa1441280e8b9
Rinat Vagapov <rvagapov550@gmail.com>
2025-12-27 02:09:07 +0700

    lesson 10

---
+++ lesson_10/leson_10_1.py
@@ -0,0 +1,63 @@
+"""
+set
+"""
+
+# set_1 = {1, 2, 3, True, "str", (5, 5)}
+# print(set_1)
+# print(type(set_1))
+
+"""
+изменяемые типы данных:
+list
+dict
+set
+"""
+
+"""
+неизменяемые типы данных:
+int
+float
+str
+tuple
+bool
+"""
+
+# set_3 = {} #здесь создастся словарь а не множество
+# set_4 = set()
+#
+# print(type(set_3))
+# print(type(set_4))
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, (1,2), (1,2)]
+# print(set(lst))
+#
+# str_1 = "fdfd"
+# print(str_1)
+#
+# range_1 = range(5)
+# print(set(range_1))
+
+set_5 = set(lst)
+# print(len(set_5))
+
+# for item in set_5:
+#     print(item)
+
+# it = iter(set_5)
+# print(next(it) )
+
+# set_5.add("fsfs")
+# print(set_5)
+#
+# set_5.update(["355", "535"])
+# print(set_5)
+#
+# set_5.remove("535")
+#
+# print(set_5)
+
+# res = set_5.issubset(set([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+# print(res)
+
+
+

12fc6695923e2d36c009827fa4c1b299c52c9fcc
Rinat Vagapov <rvagapov550@gmail.com>
2025-12-28 03:22:47 +0700

    lesson 10

---
+++ lesson_11/lesson_10_1.py
@@ -0,0 +1,48 @@
+"""
+DRY - DON/t Repeat yourself
+
+"""
+"""
+iter()
+str()
+list()
+print()
+"""
+
+# # func = print
+# # func("вывод в консоль")
+# #
+# # print(id(print))
+# # print(id(func))
+#
+# def print_text(add_text):
+#     text = f"Text for printing: {add_text}"
+#     print(text)
+#
+#
+# str_1 = "Cource by rinat"
+# int_1 = 1212
+# print("str_1", id(int_1))
+# print_text("int_1")
+#
+# iter_1 = iter(str_1)
+#
+# def summarize_two(x, y):
+#     res = x + y
+#     return res
+# sum_1 = summarize_two(1, 2)
+# print(summarize_two(1,2))
+
+
+# def is_negative(x):
+#     return x < 0
+
+# print(is_negative(3))
+# print(is_negative(0))
+# print(is_negative(-1))
+
+# lst = [1, 2, 3, -4, 5, -6, 7, -8, 9, 10]
+#
+# for item in lst:
+#     if is_negative(item):
+#         print(item, end=" ")
